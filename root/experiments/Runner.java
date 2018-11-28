4f7d95ff6b07965a84a7739d9e08dc2d0b7d10f7
hthoma20 <thoma20@up.edu>
2018-11-27 16:36:46 -0800

    ready to collect some data

--- root/experiments/Runner.java
+++ root/experiments/Runner.java
@@ -113,6 +113,7 @@ public class Runner {
     public static void main(String[] args) {
         ArrayList<TestSuite> suites = new ArrayList<>();
         for(int i : new int[]{20, 40, 80, 160}) {
+
             suites.add(new TestSuite(
                     TestSuiteConfiguration.FULL,
                     new FileResultWriterProvider(),
@@ -125,6 +126,11 @@ public class Runner {
                             new MaRzAgentProvider<>(new SuffixNodeProvider())
                     }
             ));
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         //TestSuite suite = JunoBail;
 
