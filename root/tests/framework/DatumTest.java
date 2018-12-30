ac7bca168ce3ef1236a298b18544b37f8c1bd527
Zachary Paul Faltersack <zachary.faltersack@gmail.com>
2018-12-30 13:10:38 -0800

    Test for console unit testing.

--- root/tests/framework/DatumTest.java
+++ root/tests/framework/DatumTest.java
@@ -1,6 +1,11 @@
 package framework;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
