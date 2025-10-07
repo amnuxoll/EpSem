8058286db6d3c94189f1ebfc7be346de954ec096
Landon Harrison <harrisol27@up.edu>
2025-10-07 16:24:36 -0700

    LJH: committing misc. changes to pull creation of new demo folder

---
+++ src/agents/demo/DemoAgentProvider.java
@@ -0,0 +1,27 @@
+package agents.demo;
+
+import framework.IAgent;
+import framework.IAgentProvider;
+import utils.Random;
+
+/**
+ *
+ * This agent was written as a quick study for creating a new agent with a basic
+ * heuristic for randomly selecting between a random action and the last
+ * action taken before a goal.
+ *
+ * @author Zachary Faltersack
+ */
+public class DemoAgentProvider implements IAgentProvider {
+
+    @Override
+    public IAgent getAgent() {
+        return new DemoAgent(Random.getTrue());
+    }
+
+    @Override
+    public String getAlias() {
+        return "DemoAgent1";
+    }
+}
+//for landon
