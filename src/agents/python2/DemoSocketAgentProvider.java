ed95822666b6432a4eaf19a56a519c1454b0262a
meganou <ou27@up.edu>
2025-10-07 16:21:00 -0700

    Megan Ou- Python2 Agent

---
+++ src/agents/python2/DemoSocketAgentProvider.java
@@ -0,0 +1,26 @@
+package agents.demosocketagent;
+
+import framework.IAgent;
+import framework.IAgentProvider;
+import utils.Random;
+
+/**
+ *
+ * This agent was written as a quick study for creating a new agent with a basic
+ * heuristic for randomly selecting between a random action and the last
+ * action taken before a goal.
+ *
+ * @author Zachary Faltersack
+ */
+public class DemoSocketAgentProvider implements IAgentProvider {
+
+    @Override
+    public IAgent getAgent() {
+        return new DemoSocketAgent(Random.getTrue());
+    }
+
+    @Override
+    public String getAlias() {
+        return "DemoAgent1";
+    }
+}
